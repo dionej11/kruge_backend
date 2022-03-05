3483a241ad3eff14c42e1b2200dfe67e152ea224
Juan Galvis <juangalvis.sh@gmail.com>
2022-03-05 15:57:22 -0500

    Passport JWT for Categories + id_owner in Category document

--- routes/RouterCategory.js
+++ routes/RouterCategory.js
@@ -4,44 +4,59 @@ const RouterCategory = express.Router();
 const MongoDB = require('../mongodb/client');
 const client = new MongoDB();
 
-RouterCategory.post('/new_category', async (request, response) => {
-  let result = await client.insertCategory(request.body);
-  response.json({
-      result, 
-      message: 'CATEGORY INSERTED'
-     });
-});
+const passport = require('passport');
 
-RouterCategory.get('/categories', async (request, response) => {
-  let result = await client.getAllCategories();
-  response.json({
+RouterCategory.post('/new_category',
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    let result = await client.insertCategory(request.body, request.user.sub);
+    response.json({
+      result,
+      message: 'CATEGORY INSERTED'
+    });
+  });
+
+RouterCategory.get('/categories',
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    let result = await client.getAllCategories(request.user.sub);
+    response.json({
       result,
       message: 'ALL CATEGORIES'
     });
-});
+  });
 
-RouterCategory.get('/categories/:categoryId', async (request, response) => {
-  let result = await client.getCategory(request.params.categoryId);
-  response.json({
+RouterCategory.get('/categories/:categoryId',
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    let result = await client.getCategory(request.params.categoryId, request.user.sub);
+    response.json({
       result,
-      message: 'THE CATEGORY'}
-      );
-});
+      message: 'THE CATEGORY'
+    }
+    );
+  });
 
-RouterCategory.put('/categories/:categoryId', async (request, response) => {
-  let result = await client.updateCategory(request.params.categoryId, request.body);
-  response.json({
+RouterCategory.put('/categories/:categoryId',
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    let result = await client.updateCategory(request.params.categoryId, request.body, request.user.sub);
+    response.json({
       result,
-      message: 'CATEGORY UPDATED'}
-      );
-});
+      message: 'CATEGORY UPDATED'
+    }
+    );
+  });
 
-RouterCategory.delete('/categories/:categoryId', async (request, response) => {
-  let result = await client.deleteCategory(request.params.categoryId);
-  response.json({
+RouterCategory.delete('/categories/:categoryId',
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    let result = await client.deleteCategory(request.params.categoryId, request.user.sub);
+    response.json({
       result,
-      message: 'CATEGORY DELETED'}
-      );
-});
+      message: 'CATEGORY DELETED'
+    }
+    );
+  });
 
 module.exports = RouterCategory;
